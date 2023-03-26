083f2fafb9634e8b70ff7910ded4060b00a0f87a
Trinh Cao <t.trinh.it@gmail.com>
2023-03-26 23:49:02 +0700

    Update markup

--- index.ts
+++ index.ts
@@ -5,5 +5,4 @@ const app = express()
 
 app.listen(8080)
 app.disable('x-powered')
-app.use('/assets', express.static('assets'))
-app.use('/', router)
+app.use(router)
--- router/root.ts
+++ router/root.ts
@@ -1,14 +1,15 @@
 import express from 'express'
 import engine from '../engine/engine'
+import { minify } from 'html-minifier'
 
-const router = express.Router()
+const router = express.Router({ strict: true })
 
-router.get('/:lang', async (req, res) => {
-  res.send(await engine.render('home', req.params.lang))
-})
-
-router.get('*', (req, res) => {
-  res.send('Not found')
+router.use('/', express.static('static'))
+router.use('/assets/', express.static('assets'))
+router.get('/:lang/', async (req, res) => {
+  const content = await engine.render('home', req.params.lang)
+  const minified = minify(content, { collapseWhitespace: true })
+  res.send(minified)
 })
 
 export default router
