5e2fbd3755bb49853c89f57aa6254cb70b40a5a9
Trinh <t.trinh.it@gmail.com>
2023-03-30 17:53:21 +0700

    Reorganize file structure

---
+++ controllers/page-controller.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from 'express'
+import { Liquid } from 'liquidjs'
+import { minify } from 'html-minifier'
+import vi from '../models/vi.json'
+import en from '../models/en.json'
+
+const engine = new Liquid({ extname: '.liquid', root: 'views' })
+
+async function home(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> {
+  const { lang } = req.params
+
+  if (!lang || !lang.match(/vi|en/)) {
+    return next()
+  }
+
+  const data = lang === 'en' ? en : vi
+  const page = await engine.renderFile('pages/home', { data })
+  const theme = await engine.renderFile('layout/theme', {
+    data,
+    page_content: page
+  })
+  const minified = minify(theme, { collapseWhitespace: true })
+
+  res.send(minified)
+}
+
+export default { home }
--- router/root.ts
+++ router/root.ts
@@ -1,18 +1,9 @@
 import express from 'express'
-import engine from '../engine/engine'
-import { minify } from 'html-minifier'
+import pageController from '../controllers/page-controller'
 
 const router = express.Router({ strict: true })
 
 router.use('/', express.static('static'))
-router.get('/:lang/', async (req, res, next) => {
-  const { lang } = req.params
-  if (!lang || !lang.match(/vi|en/)) {
-    return next()
-  }
-  const content = await engine.render('home', lang)
-  const minified = minify(content, { collapseWhitespace: true })
-  res.send(minified)
-})
+router.get('/:lang/', pageController.home)
 
 export default router
